6df324a3328d9e74013b2c219f2531c125f887d3
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-05-03 17:50:12 +0100

    commit tree 436fd2b5580df5ec00e0278c01b7173f0aeb57ee

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,50 @@
-# your code here
+class MySet
+
+    attr_accessor :hash
+
+    def initialize enumerable = []
+        @hash = {}
+        enumerable.each do |value|
+            hash[value] = true
+        end
+    end
+
+    def include? value
+        hash.has_key?(value)
+    end
+
+    def add value
+        hash[value] = true
+        self
+    end 
+
+    def delete value
+        hash.delete(value)
+        self
+    end
+
+    def size
+        hash.size
+    end
+    
+    def self.[](*args)
+        new(args)
+    end
+
+    def clear 
+        hash.clear
+        self
+    end
+
+    def each 
+        hash.each_key {|key| yield key}
+        self
+    end
+
+    def inspect
+        elements = hash.keys.map(&:to_s).join(', ')
+        puts( "#<MySet: {#{elements}}>" )
+        "#<MySet: {#{elements}}>"
+    end
+
+end
